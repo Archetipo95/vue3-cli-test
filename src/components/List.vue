5720485c3ee33ef344f1dd093b27e917bb4bc0cf
Martin Masevski <martin.masevski@gmail.com>
2021-10-12 19:51:40 +0200

    Initial

---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <article v-for="person in people" :key="person.id" className="person">
+      <img :src="person.image" :alt="person.name" />
+      <div>
+        <h4>{{ person.name }}</h4>
+        <p>{{ person.age }} years old</p>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    people: Array,
+  },
+};
+</script>
